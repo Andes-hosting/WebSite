296664b13dd89613cbc4043cb2a1c4d71c6bf785
zhijan <leobenitez0214@gmail.com>
2024-02-26 22:43:23 -0300

    sidebar links(TikTok & Instagram) are modified

--- website-andes-hosting/src/components/igIcon/IgIcon.js
+++ website-andes-hosting/src/components/igIcon/IgIcon.js
@@ -3,7 +3,7 @@ import igIcon from '../../img/icons/ig.svg'
 
 const IgIcon = () => {
   return (
-    <a href="https://shlink.andes-hosting.com/LvEPt"><img src={igIcon} alt="Icono instagram" /> </a>
+    <a href="https://shlink.andes-hosting.com/LvEPt"target="_blank"><img src={igIcon} alt="Icono instagram" /> </a>
   )
 }
 

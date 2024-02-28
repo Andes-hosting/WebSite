dd84495a2d6053280e36b2d7613a26fd8ef5137c
Rodrigo Rodriguez <Rodrigonbg@hotmail.com>
2024-02-28 01:18:51 -0300

    finished the style of plans' carousel

--- website-andes-hosting/src/components/gamePlansList/GamePlansList.js
+++ website-andes-hosting/src/components/gamePlansList/GamePlansList.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PlanCard from '../planCard/PlanCard'
-import { Col} from 'react-bootstrap'
 import "./GamePlansList.scss"
 
 const GamePlansList = ({game, currency}) => {
@@ -10,9 +9,7 @@ const GamePlansList = ({game, currency}) => {
 
       <div className='gameListRow'> 
         {game.plans.map((plan, index) => (
-          <Col key={index} xs={12} sm={6} lg={3}>
-            <PlanCard plan={plan} currency={currency} />
-          </Col>
+            <PlanCard key={index} plan={plan} currency={currency} />
         ))}
       </div>
     </div>

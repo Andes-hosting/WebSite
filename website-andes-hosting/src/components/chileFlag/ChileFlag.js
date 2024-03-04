da82fa66b2c76bd301fc6dfd9a38156df8a9551a
zhijan <leobenitez0214@gmail.com>
2024-03-04 19:56:20 -0300

    Location info added

---
+++ website-andes-hosting/src/components/chileFlag/ChileFlag.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import chileFlag from '../../img/Chile_flag.png'
+
+const ChileFlag = () => {
+  return (
+    <a href="#"><img className='flagChile' src={chileFlag} alt="bandera chilena" /> </a>
+  )
+}
+
+export default ChileFlag
--- website-andes-hosting/src/pages/about/AboutUs.js
+++ website-andes-hosting/src/pages/about/AboutUs.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./AboutUs.scss"
 import map from "../../img/latam.png"
 import chile from '../../img/chile.png'
+import ChileFlag from '../../components/chileFlag/ChileFlag.js'
 
 import Pterodactyl from '../../components/pterodactyl/Pterodactyl'
 import Btn from '../../components/btn/Btn'
@@ -22,10 +23,11 @@ const AboutUs = () => {
             <h3 className="location-over-title">Tenemos</h3>
             <h2 className="location-title">LA UBICACIÓN IDEAL</h2>
             <p className="location-text">Experimenta el rendimiento excepcional de nuestros servidores, combinado con una ubicación estratégica que asegura la mejor latencia en Sudamérica. Ofrecemos un servicio premium a un costo incomparable en toda la región. <span>Tu éxito es nuestra prioridad!</span></p>
+            <p className='about-location'>Santiago, Chile <ChileFlag/></p>
+            <p className='about-location-ms'>123ms</p>
           </div>
         </div>
       </div>  
-      
         <div className="about-container">
           <h2 className="about-title">¿QUIÉNES SOMOS?</h2>
           <p className="about-text">En Andes, <span>somos una comunidad apasionada y comprometida</span>. Con un equipo joven y entusiasta, nos esforzamos por proporcionar una <span>experiencia de alta calidad a un precio asequible</span>. Nuestro compromiso humano y nuestra transparencia son los pilares de nuestro negocio, y nos esforzamos por construir relaciones sólidas y duraderas con nuestros clientes. Al igual que la majestuosidad de la <span>cordillera de los Andes</span>, nuestro objetivo es <span>unir a los países sudamericanos en una comunidad de jugadores vibrante y unida</span>. ¡Únete a nosotros en este viaje y experimenta la diferencia con Andes!</p>

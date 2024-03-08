35a2c87dce9f119e01bfb3141fa51af9b812962a
Jiroma <maury0110@hotmail.com>
2024-03-08 15:00:18 -0300

    Change RRSS and banner

---
+++ website-andes-hosting/src/components/FbIcon/FbIcon.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import facebookIcon from '../../img/icons/facebook.svg'
+
+const FbIcon = () => {
+    return (
+        <a href="#"><img src={facebookIcon} alt="Icono facebook" /> </a>
+    )
+}
+
+export default FbIcon
